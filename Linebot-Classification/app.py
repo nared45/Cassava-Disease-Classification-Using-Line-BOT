cf4c86bdc6f56276e5c06c870d7551879315cd8a
Nared Fuengverojsakul <66765930+nared45@users.noreply.github.com>
2023-03-27 11:23:42 +0700

    Test model Line bot

--- Linebot-Classification/app.py
+++ Linebot-Classification/app.py
@@ -590,7 +590,7 @@ model = load_model('C:/AI4H/Flask/Line bot/Linebot-Classification/model/CASSAVA_
 # load model ResNet50 to Feature extraction
 resnet_model = ResNet50(input_shape=(image_size, image_size, 3), weights="imagenet", include_top=False, pooling='avg') 
 #load model one class classification
-occ_model = pickle.load(open("C:/Github/Cassava-Disease-Classification-Using-Line-BOT/model/OCC_SVM_VER_SUPERBIGDATA.sav", 'rb'))
+occ_model = pickle.load(open("C:/Github/Cassava-Disease-Classification-Using-Line-BOT/model/OCC_SVM_VER_SUPERBIGDATA2.sav", 'rb'))
 
 def pred_(img_path):
     # Load Image
